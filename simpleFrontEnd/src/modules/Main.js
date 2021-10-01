eb96940ed75e39a1d88704996eb0905f70f69f0c
Andrei Bogomja <bogomja@gmail.com>
2021-10-01 13:44:11 +0100

    [AB] 🐛 FIX: Dynamic API URL

--- simpleFrontEnd/src/modules/Main.js
+++ simpleFrontEnd/src/modules/Main.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 
-const API_URL = process.env.CORS_URL ?? "http://localhost:8080"
+const API_URL = process.env.REACT_APP_API_URL ?? "http://localhost:8080"
 
 function Main() {
 
