03b6769ad82237c0d062f6f6c37490fa8562a2f3
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-23 18:00:23 +0100

    red

---
+++ simpleFrontEnd/src/modules/CategoryList.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { CategoryList } from './CategoryList'
+
+describe('CategoryListShould', ()=>{
+    it('render the title', ()=>{
+        render(<CategoryList />);
+
+        const title = screen.getByText('CategoryList');
+
+        expect(title).toBeInTheDocument();
+    })
+})
