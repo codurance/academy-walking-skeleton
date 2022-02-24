ad60ae304ef38bab57049c75076b9ca92382d199
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-24 17:08:45 +0100

    redder

--- simpleWebservice/src/main/java/com/kata/SimpleWebservice/CategoryWebService.java
+++ simpleWebservice/src/main/java/com/kata/SimpleWebservice/CategoryWebService.java
@@ -15,6 +15,6 @@ public class CategoryWebService {
 
 	@PostMapping("/categories/create")
 	public void getUsersFromDatabase(CategoryCreationDto dto) {
-		throw new UnsupportedOperationException();
+		categoryService.create(dto);
 	}
 }
