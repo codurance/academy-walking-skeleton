13c4236250358d1cc29584774a299c23d3dacec4
oawood93 <oa.wood@outlook.com>
2022-02-24 11:59:39 +0000

    refactor -added router

--- simpleFrontEnd/src/index.js
+++ simpleFrontEnd/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Main } from './modules/Main';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Employees from "./pages/Employees";
+import {Main} from "./modules/Main";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/employees" element={<Employees />} />
+        </Routes>
+    </BrowserRouter>,
+  // <React.StrictMode>
+  //   <Main />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
