c6c78d359ef05b2e9df35cbbc04093ef2aff0fc4
iagardi <garbigz@gmail.com>
2022-02-24 09:57:50 +0000

    green

--- simpleFrontEnd/src/modules/Categories.js
+++ simpleFrontEnd/src/modules/Categories.js
@@ -1,8 +1,20 @@
+import {useState} from "react";
+
+
 export const Categories = () => {
+    const {categoryForm, setCategoryForm} = useState(false)
+
+    const handleNewCategoryClick = () => {
+        setCategoryForm(true)
+    }
+
     return <div>
         <h1>
             Categories
         </h1>
-        <button>new</button>
+        <button onClick={handleNewCategoryClick}>new</button>
+        {categoryForm && <div>
+            <form name = "categoryForm"></form>
+        </div>}
     </div>
 }
