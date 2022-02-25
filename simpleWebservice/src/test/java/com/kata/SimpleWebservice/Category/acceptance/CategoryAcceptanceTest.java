30f7e85fdc7e8c2718a7d7237590e3b56bb617c1
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-25 11:00:38 +0100

    red category acceptance

---
+++ simpleWebservice/src/test/java/com/kata/SimpleWebservice/Category/acceptance/CategoryAcceptanceTest.java
@@ -0,0 +1,42 @@
+package com.kata.SimpleWebservice.Category.acceptance;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+public class CategoryAcceptanceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test void
+    should_create_and_retrieve_all_the_categories_with_ok_status() throws Exception {
+
+        //Given
+        String expectedJSON = "[{\"name\":\"test category\",\"description\": \"category description\" ,\"picture\":\"picture url\"}]";
+
+
+        //WhenAndThen
+        this.mockMvc.perform(post("/categories")
+                .content("{\"name\":\"test category\",\"description\": \"category description\" ,\"picture\":\"picture url\"}"))
+                .andExpect(status().isCreated());
+
+        this.mockMvc.perform(get("/categories"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJSON));
+    }
+
+}
