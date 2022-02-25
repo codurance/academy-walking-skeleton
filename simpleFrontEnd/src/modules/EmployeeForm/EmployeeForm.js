2499d918613ff3aee7369c9254cc866f99c42d07
oawood93 <oa.wood@outlook.com>
2022-02-25 11:35:09 +0000

    green

--- simpleFrontEnd/src/modules/EmployeeForm/EmployeeForm.js
+++ simpleFrontEnd/src/modules/EmployeeForm/EmployeeForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-const EmployeeForm= ({handleForm}) => {
+const EmployeeForm= ({handleForm, employee}) => {
     const initialState = {
         reports: "",
         lastName: "",
@@ -19,7 +19,7 @@ const EmployeeForm= ({handleForm}) => {
         photo:"",
         notes:""
     }
-    const [form, setForm] = useState(initialState);
+    const [form, setForm] = useState(employee ? employee : initialState);
 
     return (
             <form data-testid="new-employee-form" role={"employee-form"} onSubmit={(event)=> {
@@ -28,7 +28,7 @@ const EmployeeForm= ({handleForm}) => {
             }}>
                 {Object.keys(form).map((field)=>{
                     return <input key={field} onChange={(event) => setForm({...form,
-                        [field]:event.target.value})} type="text" placeholder={field.toUpperCase()} role={field} />
+                        [field]:event.target.value})} type="text" placeholder={field.toUpperCase()} role={field} value={form[field]} />
                 })}
 
                 <button>Update</button>
