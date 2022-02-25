a94dcfa0bdc07b98b3b2bbde3ed11b92c9e9540e
Nicola <nicola_rushton@outlook.com>
2022-02-25 11:45:14 +0000

    red

---
+++ simpleWebservice/src/main/java/com/kata/SimpleWebservice/Category/Model/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.kata.SimpleWebservice.Category.Model;
+
+public class CategoryRepository {
+    public void createCategory(Category category) {
+        throw new UnsupportedOperationException();
+    }
+}
