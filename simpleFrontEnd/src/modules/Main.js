be508589e0a309f3f3a0472fcfd2218dc9fa9b85
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-04-07 12:51:51 +0200

    Cleanup

--- simpleFrontEnd/src/modules/Main.js
+++ simpleFrontEnd/src/modules/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL ?? "http://localhost:8080"
@@ -30,13 +30,11 @@ function Main() {
 }
 
 function User(props) {
-    return <>
-        <ul>
-            <li>Name: {props.user.name}</li>
-            <li>Age: {props.user.age}</li>
-            <li>Date of birth: {props.user.dateOfBirth}</li>
-        </ul>
-    </>;
+    return <ul>
+        <li>Name: {props.user.name}</li>
+        <li>Age: {props.user.age}</li>
+        <li>Date of birth: {props.user.dateOfBirth}</li>
+    </ul>;
 }
 
-export { Main }
+export {Main}
